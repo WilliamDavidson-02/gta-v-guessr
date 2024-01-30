cb8f2b2435eb15bd59098d053686b73a522cb755
William Davidson <wj.davidson@outlook.com>
2024-01-30 08:46:25 +0100

    Fix: Fixed protected routes getting stuck in loading state when user loggs out

--- src/lib/PrivateRoutes.tsx
+++ src/lib/PrivateRoutes.tsx
@@ -3,17 +3,17 @@ import useUserContext from "@/hooks/useUserContext";
 import { Navigate, Outlet } from "react-router-dom";
 
 export function PrivateRoutes() {
-  const { user } = useUserContext();
+  const { user, isLoading } = useUserContext();
 
-  if (!user) return <LoadingScreen />;
+  if (!user && isLoading) return <LoadingScreen />;
 
   return user ? <Outlet /> : <Navigate to="/auth/login" />;
 }
 
 export function AdminRoute() {
-  const { user } = useUserContext();
+  const { user, isLoading } = useUserContext();
 
-  if (!user) return <LoadingScreen />;
+  if (!user && isLoading) return <LoadingScreen />;
 
   return user && user.user_metadata.access_role === "admin" ? (
     <Outlet />
